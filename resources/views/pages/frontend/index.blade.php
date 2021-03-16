1d95e30017afcb06552f81c7a75d93c7c40a48bf
jackieriel <jonahjacksonj@gmail.com>
2021-03-16 11:34:21 +0100

    Before herokup push

---
+++ resources/views/pages/frontend/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.frontend')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">TESSCRIME SECURITY AGENCY</div>
+
+                <div class="card-body">
+                   <p>Welcome to Tessy Crime Reporting System</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/former', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'FrontendController@index')->name('index');
 
 Auth::routes();
 
