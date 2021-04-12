01af58e46118ef3c644c3f37f40b0cbcf643b238
jackieriel <jonahjacksonj@gmail.com>
2021-04-12 15:09:19 +0100

    Change proposed system architecture

--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">TESSCRIME SECURITY AGENCY</div>
+                <div class="card-header text-center">TESSCRIME SECURITY AGENCY</div>
 
                 <div class="card-body">
                    <p>Welcome to Tessy Crime Reporting System</p>
