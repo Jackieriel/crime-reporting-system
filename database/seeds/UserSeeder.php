31c276ff4913eb9f4a67b3548f48fa60d0d5fee7
jackieriel <jonahjacksonj@gmail.com>
2021-07-23 14:56:45 +0100

    updated user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,18 +13,30 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' =>'Tessy',
-            'email' => 'tesscrsystem@gmail.com',
-            'email_verified_at' =>Carbon::now()->format('Y-m-d H:i:s'),
-            'password' => Hash::make('password'),
-            'gender' => 'female',
-            'phone' => '08140800141',
-            'role' => 'superAdmin',
-            'photo' => 'uploads/profile/avater.jpg',
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            [
+                'name' => 'Tessy',
+                'email' => 'tesscrsystem@gmail.com',
+                'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'password' => Hash::make('password'),
+                'gender' => 'female',
+                'phone' => '08140800141',
+                'role' => 'superAdmin',
+                'photo' => 'uploads/profile/avater.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Udoh',
+                'email' => 'wanaciousfellow@gmail.com',
+                'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'password' => Hash::make('GreensoN'),
+                'gender' => 'female',
+                'phone' => '08140800141',
+                'role' => 'reporter',
+                'photo' => 'uploads/profile/avater.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]
         ]);
-
     }
 }
-
